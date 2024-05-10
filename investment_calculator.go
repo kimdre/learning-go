5e0a350f8ea6600a6264614178d324d24ea8945e
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-05-10 11:48:38 +0200

    Add function to investment calculator
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"math"
 	"fmt"
+	"math"
 )
 
+const inflationRate float64 = 2.5
+
 func main() {
-	const inflationRate float64 = 2.5
 	var investmentAmount float64
 	var expectedReturnRate float64
 	var years float64
+
+	fmt.Println("-- Investment Calculator --")
 
 	fmt.Print("Enter investment amount: ")
 	fmt.Scan(&investmentAmount)
@@ -20,9 +23,15 @@ func main() {
 	fmt.Print("Enter number of years: ")
 	fmt.Scan(&years)
 
-	futureValue :=  investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
-	fmt.Println("Expected return value:", futureValue)
-	fmt.Println("Future real value:", futureRealValue)
+	fmt.Printf("Expected return value: %.2f €\n", futureValue)
+	fmt.Printf("Future real value: %.2f €\n", futureRealValue)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	frv := fv / math.Pow(1+inflationRate/100, years)
+
+	return fv, frv
 }
