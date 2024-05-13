9bd4862bd6f2a0b11c6cd7b9e019739e18bb7138
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-05-13 18:59:44 +0200

    pointers
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

---
+++ pointers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32
+
+	var agePointer *int
+	agePointer = &age
+
+	fmt.Println("Age:", age)
+	getAdultYears(agePointer)
+	fmt.Println(age)
+}
+
+func getAdultYears(age *int) {
+	// return *age - 18
+	*age = *age - 18
+}
