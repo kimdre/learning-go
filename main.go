196633bdeb3ed012ac8711741932c2ee7e6d7992
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-05-17 18:35:19 +0200

    type aliases
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main()  {
-	userNames := make([]string, 2, 5)
+	userNames := make([]string, 2, 5) // 2: initial array length, 5: capacity/max array length
 
 	userNames[0] = "Julie"
 	// userNames[6] = "Luka"  <-- Would crash because out of bounce (higher than capacity defined in make() -> 5)
@@ -12,4 +18,12 @@ func main()  {
 	userNames = append(userNames, "Tobi")
 
 	fmt.Println(userNames)
+
+	courseRating := make(floatMap, 2)
+
+	courseRating["go"] = 4.7
+	courseRating["react"] = 4.8
+	courseRating["test"] = 2
+
+	courseRating.output()
 }
