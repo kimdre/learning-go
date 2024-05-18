5091a0c6044f25d9878505aa603f9948377884b6
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-05-18 11:56:45 +0200

    recusrion
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- main.go
+++ main.go
@@ -3,37 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3}
-
-	double := createTransformer(2)
-	triple := createTransformer(3)
-
-	transformed := transformNumbers(&numbers, func(number int) int {
-		return number * 2
-	} )
-
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
-
-	fmt.Println(transformed)
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	fact := factorial(6)
+	fmt.Println(fact)
 }
 
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
-	dNumbers := []int{}
-
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transform(val))
+func factorial(number int) int {
+	if number == 0 {
+		return 1
 	}
+	return number * factorial(number -1)
 
-	return dNumbers
-}
-
-
-// Create function based on given factor
-func createTransformer(factor int) func(int) int {
-	return func(number int) int {
-		return number * factor
-	}
+	// result := 1
+	// for i := 1; i <= number; i++ {
+	// 	result = result * i
+	// }
+	// return result
 }
