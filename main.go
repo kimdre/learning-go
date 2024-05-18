adfddd17347aa700f6effb38ff1a06cb4d5e530b
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-05-18 11:32:45 +0200

    Returning functions as values
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- main.go
+++ main.go
@@ -8,9 +8,18 @@ type transformFunc func(int) int
 
 func main()  {
 	numbers := []int{1,2,3,4}
+	moreNumbers:= []int{5,1,2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(doubled, tripled)
+
+	transorfmerFn1 := getTransformerFunction(&numbers)
+	transorfmerFn2 := getTransformerFunction(&moreNumbers)
+
+	tranformedNumbers := transformNumbers(&numbers, transorfmerFn1)
+	moreTranformedNumbers := transformNumbers(&numbers, transorfmerFn2)
+	fmt.Println(tranformedNumbers)
+	fmt.Println(moreTranformedNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFunc) []int {
@@ -20,6 +29,14 @@ func transformNumbers(numbers *[]int, transform transformFunc) []int {
 	}
 
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFunc {
+	if(*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
